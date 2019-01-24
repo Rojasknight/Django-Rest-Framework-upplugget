f7f0a5fc498810a319465c8ebd3af62f6cd2ee9a
Rojas Reyes <danny.rojas.reyes@accenture.com>
2019-01-24 15:18:12 -0500

    finished

---
+++ superheroes/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+class Publisher(models.Model):
+    name = models.CharField(max_length=100, blank=True, default='')
+    founder = models.CharField(max_length=100, blank=True, default='')
+
+    class Meta:
+        ordering = ('name',)
+
+    def __str__(self):
+        return self.name
+
+class SuperHeroe(models.Model):
+    name = models.CharField(max_length=100, blank=True, default='')
+    gender = models.CharField(max_length=100, blank=True, default='')
+    real_name = models.CharField(max_length=100, blank=True, default='')
+    publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ('name',)
+
+    def __str__(self):
+        return self.name
---
+++ superheroes/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url, include
+from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
+from superheroes import views
+
+urlpatterns = [
+    path('/superheroe/', views.SuperHeroeList.as_view()),
+    path('/superheroe/<int:pk>/', views.SuperHeroeDetail.as_view()),
+
+    path('/publisher/', views.PublisherList.as_view()),
+
+]
